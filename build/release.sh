3ec7de29f9e098bfa4be36eb775a92a7c21fa8d7
heora <yangzhiqiang@winwin-inc.com>
2022-04-29 14:40:49 +0800

    build: compile 2.0.5

---
+++ build/release.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  if [[ `git status --porcelain` ]]; 
+  then
+    git add -A
+    git commit -am "build: compile $VERSION"
+  fi
+
+  # commit
+  npm version $VERSION --message "release: $VERSION"
+
+  # publish
+  git push origin master
+  git push origin refs/tags/v$VERSION
+
+  if [[ $VERSION =~ [beta] ]]
+  then
+    npm publish --tag beta
+  else 
+    npm publish
+  fi
+fi
